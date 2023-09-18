b8810f0d2709a7a054d884cd4e060b71f7cab553
Lwall02 <liam.wall@mail.utoronto.ca>
2023-09-18 18:55:12 -0400

    The TreeMultiSet interface

---
+++ src/TreeMultiSet.java
@@ -0,0 +1,16 @@
+public interface TreeMultiSet {
+
+    public void Tree();
+
+    public TreeMultiSet add(int x);
+
+    public TreeMultiSet remove(int x);
+
+    public TreeMultiSet contains(int x);
+
+    public TreeMultiSet is_empty();
+
+    public TreeMultiSet count(int x);
+
+    public TreeMultiSet size();
+}
